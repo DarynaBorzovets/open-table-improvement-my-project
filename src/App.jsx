704e1614a80cf6aa58b22024230ad7116248a655
DarynaBorzovets <biz.daryna@gmail.com>
2024-03-26 19:25:32 -0400

    Navbar.jsx: Links added, App.jsx: Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import GuestList from './components/GuestList';
-import VIPList from './components/VIPList'
-import Navbar from './components/Navbar'
-import HomePage from './pages/HomePage'
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+
 const App = () => {
   return (
-       <>
-       <Navbar/>
-       <HomePage/>
-        <GuestList/>
-        <VIPList/>
-        </>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/project" element={<GuestList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
-
-
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
 
 
 function Navbar() {
   return (
     <nav className="bg-gray-800 py-4">
     <div className="max-w-7xl mx-auto px-4 flex justify-center items-center">
-      <a href="#" className="text-white text-lg mr-8">Problem & Solution</a>
-      <a href="#" className="text-white text-lg">Project</a>
+  
+    <Link to="/" className="text-white text-lg mr-8">Problem & Solution</Link>
+    <Link to="/project" className="text-white text-lg mr-8">Project</Link>
     </div>
   </nav>
   
