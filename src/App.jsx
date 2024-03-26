fdee1a77ebe6a05d5cfe8af1c283d0b2fabe382c
DarynaBorzovets <biz.daryna@gmail.com>
2024-03-26 18:57:45 -0400

    HomePage.jsx created

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './index.css';
 import GuestList from './components/GuestList';
 import VIPList from './components/VIPList'
 import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
 const App = () => {
   return (
        <>
        <Navbar/>
+       <HomePage/>
         <GuestList/>
         <VIPList/>
         </>
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+
+
+const Home = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="max-w-screen-xl p-8 bg-white rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold mb-4">Problem</h1>
+        <p className="text-lg mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo nunc non risus laoreet, a posuere magna suscipit.
+        </p><br/>
+        <h1 className="text-3xl font-bold mb-4">Solution</h1>
+
+        <p className="text-lg mb-4">
+          Nulla facilisi. Duis sed mauris a ligula sodales ultrices. Proin ut tellus non leo volutpat tempus.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
